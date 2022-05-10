84efb4a78a5cb6ea46fdfc34c630cb1eff44bbce
DS-PRO-PIT <104715969+DS-PRO-PIT@users.noreply.github.com>
2022-05-10 19:42:02 +0300

    Update entrypoint.sh

--- reproduce/entrypoint.sh
+++ reproduce/entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh -l
 
-sh -c "echo cat /etc/os-release"
+sh -c "echo"
+sh -c "echo #cat /etc/os-release"
 sh -c "cat /etc/os-release"
+
+sh -c "echo"
+sh -c "echo #lsb_release -a"
 sh -c "lsb_release -a"
+
+sh -c "echo"
+sh -c "echo #hostnamectl"
 sh -c "hostnamectl"
+
+sh -c "echo"
+sh -c "echo #uname -r"
 sh -c "uname -r"
