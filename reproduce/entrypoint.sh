2f7def212b840bba792c4c68f8982932dcab10d4
DS-PRO-PIT <104715969+DS-PRO-PIT@users.noreply.github.com>
2022-05-15 17:53:14 +0300

    Update entrypoint.sh

--- reproduce/entrypoint.sh
+++ reproduce/entrypoint.sh
@@ -1,26 +1,19 @@
 #!/bin/sh -l
 
-#sh -c "echo"
-#sh -c "echo \#cat /etc/os-release"
-#sh -c "cat /etc/os-release"
-
-#sh -c "echo"
-#sh -c "echo \#uname -r"
-#sh -c "uname -r"
-
-#sh -c "echo"
-#sh -c "echo \#python --version"
-#sh -c "python --version"
-
 python the_game.py
 
-pip freeze --no-cache-dir
+sh -c "echo"
+sh -c "echo \#cat /etc/os-release"
+sh -c "cat /etc/os-release"
 
-#sh -c "echo"
-#sh -c "echo \#pip freeze"
-#sh -c "pwd"
-#sh -c "pip --version"
-#pip freeze --no-cache-dir > requirements.txt
-#sh -c "pip freeze"
-#sh -c "pip freeze >&1"
-#ls -l
+sh -c "echo"
+sh -c "echo \#uname -r"
+sh -c "uname -r"
+
+sh -c "echo"
+sh -c "echo \#python --version"
+sh -c "python --version"
+
+echo
+echo \#pip freeze --no-cache-dir
+pip freeze --no-cache-dir
