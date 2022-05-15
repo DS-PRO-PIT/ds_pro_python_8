779b9c43592279caedd32c66007f0578a6730fe4
DS-PRO-PIT <104715969+DS-PRO-PIT@users.noreply.github.com>
2022-05-15 17:42:25 +0300

    Update entrypoint.sh

--- reproduce/entrypoint.sh
+++ reproduce/entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/sh -l
 
-sh -c "echo"
-sh -c "echo \#cat /etc/os-release"
-sh -c "cat /etc/os-release"
+#sh -c "echo"
+#sh -c "echo \#cat /etc/os-release"
+#sh -c "cat /etc/os-release"
 
-sh -c "echo"
-sh -c "echo \#uname -r"
-sh -c "uname -r"
+#sh -c "echo"
+#sh -c "echo \#uname -r"
+#sh -c "uname -r"
 
-sh -c "echo"
-sh -c "echo \#python --version"
-sh -c "python --version"
+#sh -c "echo"
+#sh -c "echo \#python --version"
+#sh -c "python --version"
+
+python the_game.py
+
 
 #sh -c "echo"
 #sh -c "echo \#pip freeze"
