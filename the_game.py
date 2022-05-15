93484841e8e6e324cd679e65991cee443f048565
DS-PRO-PIT <DS-PRO-PIT@yandex.ru>
2022-05-15 17:36:42 +0300

    Add the_game.py

---
+++ the_game.py
@@ -0,0 +1,52 @@
+
+from game_v2 import random_predict
+from result_game import random_binary_search, binary_search
+from result_game import reveal_max_attempts, score_attempts
+
+print()
+print('random_predict (score_attempts):')
+score_attempts(random_predict)
+score_attempts(random_predict)
+score_attempts(random_predict)
+score_attempts(random_predict)
+score_attempts(random_predict)
+
+print()
+print('random_binary_search (score_attempts):')
+score_attempts(random_binary_search)
+score_attempts(random_binary_search)
+score_attempts(random_binary_search)
+score_attempts(random_binary_search)
+score_attempts(random_binary_search)
+
+print()
+print('binary_search (score_attempts):')
+score_attempts(binary_search)
+score_attempts(binary_search)
+score_attempts(binary_search)
+score_attempts(binary_search)
+score_attempts(binary_search)
+
+print()
+print('random_predict (reveal_max_attempts):')
+reveal_max_attempts(random_predict)
+reveal_max_attempts(random_predict)
+reveal_max_attempts(random_predict)
+reveal_max_attempts(random_predict)
+reveal_max_attempts(random_predict)
+
+print()
+print('random_binary_search (reveal_max_attempts):')
+reveal_max_attempts(random_binary_search)
+reveal_max_attempts(random_binary_search)
+reveal_max_attempts(random_binary_search)
+reveal_max_attempts(random_binary_search)
+reveal_max_attempts(random_binary_search)
+
+print()
+print('binary_search (reveal_max_attempts):')
+reveal_max_attempts(binary_search)
+reveal_max_attempts(binary_search)
+reveal_max_attempts(binary_search)
+reveal_max_attempts(binary_search)
+reveal_max_attempts(binary_search)
